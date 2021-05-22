1d5baf7cb54572c9d23c1f16381127fb5a2c5dac
yuliang <lench870888@hotmail.com>
2021-05-22 20:47:29 +0800

    Signed-off-by: yuliang <lench870888@hotmail.com>

--- build-ZBY_MQQ-Desktop_Qt_5_12_9_MinGW_64_bit-Release/ZBY_MQ/ui_mainwindow.h
+++ build-ZBY_MQQ-Desktop_Qt_5_12_9_MinGW_64_bit-Release/ZBY_MQ/ui_mainwindow.h
@@ -10,6 +10,7 @@
 #define UI_MAINWINDOW_H
 
 #include <QtCore/QVariant>
+#include <QtGui/QIcon>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QGridLayout>
 #include <QtWidgets/QGroupBox>
@@ -40,6 +41,9 @@ public:
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
         MainWindow->resize(800, 600);
+        QIcon icon;
+        icon.addFile(QString::fromUtf8("../../Container_Multiple_Channels_20200522/Container_Multiple_Channels_20200522/Main/ICO/ICO.ico"), QSize(), QIcon::Normal, QIcon::Off);
+        MainWindow->setWindowIcon(icon);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         gridLayout = new QGridLayout(centralwidget);
